d8df5d6999449ca6ecb0138d8fe497f1866b9570
Dhia Eddine MOKHTAR <151074723+DhiaMokhtar@users.noreply.github.com>
2025-08-10 07:13:50 +0100

    jenkinsfile done V20

--- frond-end/src/app/guards/auth.guard.ts
+++ frond-end/src/app/guards/auth.guard.ts
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.isLoggedIn()) {
       return true;
     }
-    this.router.navigate(['/auth']);
+    this.router.navigate(['/auth']); // was '/login'
     return false;
   }
 }
