32eabe94d95879ff24ffaaf2fd9c54f03ebb55f1
Dhia Eddine MOKHTAR <151074723+DhiaMokhtar@users.noreply.github.com>
2025-08-16 23:32:54 +0100

    certs problem

--- microservice/courses/entrypoint.sh
+++ microservice/courses/entrypoint.sh
@@ -17,16 +17,10 @@ python manage.py collectstatic --noinput
 echo "[courses] SSL setup"
 mkdir -p /app/ssl
 rm -f /app/ssl/localhost.pem /app/ssl/localhost-key.pem
-if [[ -f /certs-in/localhost.pem && -f /certs-in/localhost-key.pem ]]; then
-  install -m 600 /certs-in/localhost.pem /app/ssl/localhost.pem
-  install -m 600 /certs-in/localhost-key.pem /app/ssl/localhost-key.pem
-else
-  echo "[courses] Generating self-signed cert"
-  openssl req -x509 -nodes -newkey rsa:2048 -days 365 \
-    -keyout /app/ssl/localhost-key.pem \
-    -out /app/ssl/localhost.pem \
-    -subj "/CN=localhost"
-fi
+
+install -m 600 /certs-in/localhost.pem /app/ssl/localhost.pem
+install -m 600 /certs-in/localhost-key.pem /app/ssl/localhost-key.pem
+
 ls -l /app/ssl
 echo "[courses] Starting HTTPS server"
 exec python manage.py runsslserver 0.0.0.0:8000 --certificate /app/ssl/localhost.pem --key /app/ssl/localhost-key.pem
